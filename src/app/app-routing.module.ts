302dfba8ef59bc053134a3f8b0afd90db2b19c25
zmla03191 <ext.marie-aure.liza@sncf.fr>
2020-02-14 17:36:09 +0100

    Intitialisation

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AccueilComponent } from './components/accueil/accueil.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: 'accueil', pathMatch: 'full' },
+  { path: 'accueil', component: AccueilComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
