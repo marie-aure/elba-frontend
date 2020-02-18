59e9c620fe55687febd92135cc890709f817360c
marie-aure <marieaureliza@wanadoo.fr>
2020-02-18 01:12:01 +0100

    [T001][T002][T003] ajout des composants accueil - main - erreur

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.less']
+})
+export class MainComponent implements OnInit {
+  titre = 'lol';
+
+  constructor() { 
+
+
+
+  }
+
+  ngOnInit() {
+  }
+
+}
